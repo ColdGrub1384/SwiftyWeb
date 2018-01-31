ab31d5bfa8e1a4d28c58dcae9720d235254a7ef4
Adrian Labbe <adrilabbelol@gmail.com>
2018-01-31 11:42:06 -0300

    Updated documentation

--- SwiftyHTM.swift
+++ SwiftyHTM.swift
@@ -1,38 +1,32 @@
 public extension String {
 
   /// Print HTML from given tag, attributes and inner.
-  ///
-  /// - Parameters:
-  ///   - attributes: Attributes of tag, default value is `[]`.
-  ///
-  ///   Example:
+  ///   Examples:
   ///
   ///       "a".htm("href='https://www.google.com'")
   ///       // <a href='https://www.google.com'></a>
   ///
+  ///       "div".htm() {
+  ///         "p".htm() {
+  ///           print("Hello World!")
+  ///         }
+  ///       }
+  ///       /* <div>
+  ///         <p>
+  ///         Hello World!
+  ///         </p>
+  ///         </div>
+  ///       */
+  ///
+  ///       "br".htm(single: true)
+  ///       // <br>
+  ///
+  /// - Parameters:
+  ///   - attributes: Attributes of tag, default value is `[]`.
+  ///
   ///   - single: If the tag is single, default is false.
   ///
-  ///   Example:
-  ///
-  ///     "br".htm(single: true)
-  ///     // <br>
-  ///
   ///   - inner: Code executed before closing the tag, use it to print inner html.
-  ///
-  ///   Example:
-  ///
-  ///     "div".htm() {
-  ///       "p".htm() {
-  ///         print("Hello World!")
-  ///       }
-  ///     }
-  ///     /* <div>
-  ///        <p>
-  ///        Hello World!
-  ///        </p>
-  ///        </div>
-  ///     */
-  ///
   public func htm(_ attributes: [String] = [], single: Bool = false, inner:(() -> Void)? = nil) {
     
     var attributesString = ""
