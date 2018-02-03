8bd22e9091c8b5e971d16c5d6837c5c0b6adbf66
Adrian Labbe <adrilabbelol@gmail.com>
2018-02-03 12:33:09 -0300

    Fixed Dash XML URL

--- build-documentation.sh
+++ build-documentation.sh
@@ -1,3 +1,3 @@
-jazzy --module SwiftyWeb -d https://coldgrub1384.github.io/SwiftyWeb/docsets/Pisth.xml -g https://github.com/ColdGrub1384/SwiftyWeb
+jazzy --module SwiftyWeb -d https://coldgrub1384.github.io/SwiftyWeb/docsets/SwiftyWeb.xml -g https://github.com/ColdGrub1384/SwiftyWeb
 rm -r build
 
